60d9c8c32c2f4ed417f900e0466b37dbd6813cc2
Jurgen de Vries <jurgen.de.vries@ordina.nl>
2017-06-29 19:50:56 +0200

    code aanpassing

--- test/normalize.test.js
+++ test/normalize.test.js
@@ -2,12 +2,12 @@ describe('Normalizer', function() {
     it("accepts data", function () {
     });
 
-    // make a failing test for this and let them fix it.
     it("does not accept incomplete  data", function () {
-        // Invoke the unit being tested as necessary
         var json = '{"PersonalIdentifier": 2111858}';
-        var norm = normalizeData(json);
+        var testNormalize = function() {
+            normalizeData(json)
+        }
 
-        expect(normalizeData).toThrowError("Data incomplete");
+        expect(testNormalize).toThrowError("Data incomplete");
     });
 });
